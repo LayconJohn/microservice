67aeb6ab39ea069c0ebd7193e3aa43eadf4cb4e1
layconjohn <layconjohn@gmail.com>
2024-12-07 23:49:38 -0300

    feat(checkout): create CourseRepositoryDatabase

--- services/checkout/test/main.test.ts
+++ services/checkout/test/main.test.ts
@@ -1,9 +1,10 @@
 import Checkout from "../src/application/usecase/Checkout";
+import CourseRepositoryDatabase from "../src/infra/repository/CourseRepositoryDatabase";
 import OrderRepositoryDatabase from "../src/infra/repository/OrderRepositoryDatabase";
 
 test("Deve realizar o checkout", async function() {
     const orderRepository = new OrderRepositoryDatabase();
-    const courseRepository = new CourseRepository();
+    const courseRepository = new CourseRepositoryDatabase();
     const checkout = new Checkout(orderRepository, courseRepository);
     const input = {
         courseId: "83e88f3a-49a5-43e0-a07a-8dd9e64c0915",
